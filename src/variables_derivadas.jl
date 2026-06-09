5c29e21a38099503c18303faf5019796ce30d53a
jcamenAtUJI <jcamen@lsi.uji.es>
2026-06-09 11:47:32 +0200

    función hay_exitus_episodio terminada y pequeños cambios en la conexión a la BBDD

--- src/variables_derivadas.jl
+++ src/variables_derivadas.jl
@@ -1,3 +1,12 @@
-function hayExitusEpisodio()
-    
+using DataFrames
+
+include("conexion_bbdd.jl")
+
+function hay_exitus_episodio(tabla::DataFrame)::DataFrame
+    resultado = DataFrame()
+    # Se copia tal cual la columna de id_anonim_episodio
+    resultado.id_anonim_episodio = tabla.id_anonim_episodio
+    # Se escribe 0 (falso) o 1 (verdadero) según si ha habido exitus o no
+    resultado.hay_exitus = Int.(tabla.motivo_alta_pk .== 4)
+    return resultado
 end
