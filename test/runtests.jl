25a7b9218646a68bd5326c4f8b45e118859437b5
Óscar Belmonte Fernández. <oscar.belmonte@uji.es>
2026-06-15 10:30:49 +0200

    Cambio en los tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using GiantFtr
 using Test
 
 @testset "GiantFtr.jl" begin
-    @test_skip size(GiantFtr.get_ftr_hosp("../credenciales.json")) == (9304, 16) 
-    @test GiantFtr.categoria_barthel(144350628, "../credenciales.json") == "Dependencia severa"
-    @test GiantFtr.categoria_barthel(143182955, "../credenciales.json") == "Dependencia leve"
-    @test GiantFtr.categoria_barthel(143155772, "../credenciales.json") == "Dependencia moderada"
+    @test_skip size(get_ftr_hosp("../credenciales.json")) == (9304, 16) 
+    @test categoria_barthel(144350628, "../credenciales.json") == "Dependencia severa"
+    @test categoria_barthel(143182955, "../credenciales.json") == "Dependencia leve"
+    @test categoria_barthel(143155772, "../credenciales.json") == "Dependencia moderada"
 end
