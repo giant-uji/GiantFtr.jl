2a34aa5b6395253573297cac4d60b5e9ec9f2961
Carlos Cano Genoves <cgenoves@uji.es>
2026-08-02 17:46:53 +0200

    preparacion en curso

--- src/GiantFtr.jl
+++ src/GiantFtr.jl
@@ -1,5 +1,25 @@
 module GiantFtr
 
-include("conexion_bbdd.jl")
+using DataFrames
+using LibPQ: Connection                 #Acceder a DB
+using JSON: parsefile                   #Leer fichero de credenciales
+using CSV                               #Cargar fichero CSV
+using Base.Filesystem: isfile           #Comprobar si existe fichero
+using Dates: DateTime, Millisecond      #Columnas tipo Fecha
+using CategoricalArrays: categorical    #Columnas tipo Categorica (optimizacion)
+
+include("utilidades/conexion_bbdd.jl")
+
+#PREPARACION (carga datos, tipo columnas y limpieza)
+include("preparacion/ftr_hosp.jl")
+include("preparacion/ftr_hosp_valenf.jl")
+include("preparacion/ftr_hosp_codificacion.jl")
+include("preparacion/ftr_hosp_interconsultas.jl")
+include("preparacion/ftr_hosp_int_quirurgicas.jl")
+include("preparacion/ftr_hosp_traslados.jl")
+include("preparacion/ftr_hosp_ctes.jl")
+
+#FALTA: Laboratorio y resultados
+#Falta: URGENCIAS (de lo anterior)
 
 end
