08d7dc1b468afccc5371d59c0d8748309dacfef6
Carlos Cano Genoves <cgenoves@uji.es>
2026-08-29 14:54:48 +0200

    Variable derivada horas_valenf_previa

--- src/variables/ftr_hosp_valenf/basica.jl
+++ src/variables/ftr_hosp_valenf/basica.jl
@@ -11,3 +11,19 @@ end
 
 export error_umbral
 registrar!(error_umbral;dependencias = [get_ftr_hosp_valenf], usa = [VALENF], extiende = (VALENF, :id))
+
+
+function horas_valenf_previa(df_valenf::DataFrame)::DataFrame
+    valenf_mod = select(df_valenf, :id, :id_anonim_episodio, :fecha_valoracion; copycols=true)
+
+    #Se ordena por episodio y fecha
+    sort!(valenf_mod, [:id_anonim_episodio, :fecha_valoracion])
+
+    transform!(groupby(valenf_mod, :id_anonim_episodio),
+    :fecha_valoracion => (x -> [missing; Int.(ceil.(Dates.value.(diff(x)) ./ 3_600_000))]) => :horas_valenf_previa)
+
+    return select(valenf_mod, :id, :horas_valenf_previa)
+end
+
+export horas_valenf_previa
+registrar!(horas_valenf_previa;dependencias = [get_ftr_hosp_valenf], usa = [VALENF], extiende = (VALENF, :id))
