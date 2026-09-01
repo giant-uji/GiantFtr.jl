ecc8cd252acbac002f611d8ee49d0b36bdb3bb5e
Carlos Cano Genoves <cgenoves@uji.es>
2026-09-01 12:37:48 +0200

    Variable derivada orden_valenf

--- src/variables/ftr_hosp_valenf/basica.jl
+++ src/variables/ftr_hosp_valenf/basica.jl
@@ -14,7 +14,7 @@ registrar!(error_umbral;dependencias = [get_ftr_hosp_valenf], usa = [VALENF], ex
 
 
 function horas_valenf_previa(df_valenf::DataFrame)::DataFrame
-    valenf_mod = select(df_valenf, :id, :id_anonim_episodio, :fecha_valoracion; copycols=true)
+    valenf_mod = select(df_valenf, :id, :id_anonim_episodio, :fecha_valoracion)
 
     #Se ordena por episodio y fecha
     sort!(valenf_mod, [:id_anonim_episodio, :fecha_valoracion])
@@ -27,3 +27,17 @@ end
 
 export horas_valenf_previa
 registrar!(horas_valenf_previa;dependencias = [get_ftr_hosp_valenf], usa = [VALENF], extiende = (VALENF, :id))
+
+function orden_valenf(df_valenf::DataFrame)::DataFrame
+    valenf_mod = select(df_valenf, :id, :id_anonim_episodio, :fecha_valoracion)
+    
+    sort!(valenf_mod, [:id_anonim_episodio, :fecha_valoracion])
+    
+    transform!(groupby(valenf_mod, :id_anonim_episodio),
+               :id_anonim_episodio => eachindex => :orden_valenf)
+    
+    return select(valenf_mod, :id, :orden_valenf)
+end
+
+export orden_valenf
+registrar!(orden_valenf;dependencias = [get_ftr_hosp_valenf], usa = [VALENF], extiende = (VALENF, :id))
